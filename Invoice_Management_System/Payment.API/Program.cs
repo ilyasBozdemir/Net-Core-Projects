4f9b7a69471f56147105b37cc21e1e1e53f23ec4
ilyasBozdemir <Bozdemir.ib70@gmail.com>
2022-07-05 23:39:49 +0300

    updated

--- Invoice_Management_System/Payment.API/Program.cs
+++ Invoice_Management_System/Payment.API/Program.cs
@@ -1,11 +1,34 @@
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHttpsRedirection(opt => opt.HttpsPort = 7278);
+
+#region CORS
+
+builder.Services.AddCors();
+
+string corsUrl = builder.Configuration["Cors:site"];
+string[] corsUrls;
+if (corsUrl.Contains(","))
+    corsUrls = corsUrl.Split(',').ToArray();
+else
+{
+    corsUrls = new string[1];
+    corsUrls[0] = corsUrl;
+}
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("WebApp",
+        builder =>
+        {
+            builder.WithOrigins(corsUrls)
+                                .AllowAnyHeader()
+                                .AllowAnyMethod();
+        });
+});
+
+#endregion
 
 var app = builder.Build();
 
@@ -16,9 +39,18 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseCors();
 
 app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+app.UseHttpsRedirection();
 
 app.MapControllers();
 
