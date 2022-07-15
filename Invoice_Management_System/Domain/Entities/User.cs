48733377d7c5441ef8fa51c2c78864907ead9330
ilyasBozdemir <Bozdemir.ib70@gmail.com>
2022-07-15 15:20:02 +0300

    updated

--- Invoice_Management_System/Domain/Entities/User.cs
+++ Invoice_Management_System/Domain/Entities/User.cs
@@ -7,6 +7,7 @@ namespace Domain.Entities
         public long? CitizenId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string ProfilePictures { get; set; }
         public string FullName { get => $"{FirstName} {LastName}"; }
         public DateTime BirthDay { get; set; }
         public Gender Gender { get; set; }
